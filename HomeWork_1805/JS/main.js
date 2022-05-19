386bd48e24b3e6513a7e63e057cc4f0190f3637e
NazMzadeh <100687473+Nz020708@users.noreply.github.com>
2022-05-19 04:33:59 +0400

    Add files via upload

---
+++ HomeWork_1805/JS/main.js
@@ -0,0 +1,20 @@
+function Show(text) {
+let count=0;
+for (let i = 0; i < text.length; i++) {
+ for (let j = 0; j < text.length; j++) {
+  if(text[i]==text[j]&& i>j){
+    break;
+  }
+  if (text[i]==text[j]) {
+    count++;
+  }
+ }  
+ if (count>0) {
+  console.log(`${text[i]}-->Count:${count}`);
+
+ }
+
+}
+}
+Show("Have a good day.");
+
